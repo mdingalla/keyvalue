72c28e05b88ab4eb3cf8c785b00e34bf6a60bad9
Mark Dave Incorporado INGALLA <mark.ingalla@sg.interplex.com>
2018-04-19 11:20:33 +0800

    mlab connection

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export default callback => {
 
-	mongoose.connect('mongodb://localhost:27017/test')
-		
+	// mongoose.connect('mongodb://localhost:27017/test')
+	mongoose.connect('mongodb://mdingalla:pass1234@ds149309.mlab.com:49309/freetest')		
 	callback(mongoose);
 }
